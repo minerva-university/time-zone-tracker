7fb3ad96c36271e8f8ec16c271be12ed47230619
Ivka1303 <kreshchenetska.ivanna@gmail.com>
2023-04-18 00:06:36 +0200

    updating the name of the Python web package, so there's no conflict with the directory name

--- web/db_models.py
+++ web/db_models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Text, Integer, Date, Boolean, ForeignKey, String, DateTime
 from datetime import datetime
-from web import db
+from web.db import db
 
 
 class User(db.Model):
