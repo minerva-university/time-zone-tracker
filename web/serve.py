48422b87a128df06b20be7df954bcf1b683d7903
Ivka1303 <kreshchenetska.ivanna@gmail.com>
2023-04-17 23:59:40 +0200

    adding web to requirements.txt and correcting env var for db

--- web/serve.py
+++ web/serve.py
@@ -8,7 +8,7 @@ from .db_models import *
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
