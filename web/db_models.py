22c727e3dff8d42081121424291d60f24315e052
trinhnguyen1810 <105156592+trinhnguyen1810@users.noreply.github.com>
2023-04-21 19:19:18 +0530

    Update db_models.py

--- web/db_models.py
+++ web/db_models.py
@@ -7,16 +7,22 @@ class User(db.Model):
     #creating user table to be able to log people in
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50))
-    username = db.Column(db.String(50))
+    #get email to properly authenticate calendar
+    #index for email and user as we are going to query a lot (even for friend search)
+    email = db.Column(db.String(50),index = True)
+    username = db.Column(db.String(50),index=True)
     password = db.Column(db.String(20))
     timezone = db.Column(db.String(10))
     location = db.Column(db.String(50)) 
+    #get user's status to check availability time
     status = db.Column(db.String(20))  
+    #salt for more secure password
     salt = db.Column(db.String)
+    #check to see if the calendar is authenticated or not
     calendar_authenticated = db.Column(db.Boolean, default=False)
+    #give users notifications to those they havent talked much
     reminder_frequency = db.Column(db.Integer, default=7 * 24 * 60 * 60)
-    last_reminder_date = db.Column(db.DateTime(timezone=True))
+    last_reminder_date = db.Column(db.DateTime(timezone=True,index = True))
     
     def __repr__(self):
         return (f'<User(id={self.id}, email={self.email}, '
@@ -33,7 +39,7 @@ class Friend(db.Model):
     #creating friends table to display overlapping times
     __tablename__ = 'friends'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),index=True)
     friend_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
@@ -47,6 +53,7 @@ class Interaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     friend_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    #index as we are going to query datetime a lot to display less interacted users
     last_interaction_date =  db.Column(db.DateTime(timezone=True),index=True)
 
     def __repr__(self):
